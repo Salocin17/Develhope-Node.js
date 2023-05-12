4848e9ade97e0e420c7f36941eb10b7a6df104bf
Nicolas Zarone <nikolaszarone@gmail.com>
2023-05-12 15:01:51 +0200

    figlet

---
+++ Server.mjs
@@ -0,0 +1,18 @@
+import figlet from "figlet";
+
+figlet("Hello World!!", function (err, data) {
+  if (err) {
+    console.log("Something went wrong...");
+    console.dir(err);
+    return;
+  }
+  console.log(data);
+});
+
+//  _   _      _ _        __        __         _     _ _ _ 
+//  | | | | ___| | | ___   \ \      / /__  _ __| | __| | | |
+//  | |_| |/ _ \ | |/ _ \   \ \ /\ / / _ \| '__| |/ _` | | |
+//  |  _  |  __/ | | (_) |   \ V  V / (_) | |  | | (_| |_|_|
+//  |_| |_|\___|_|_|\___/     \_/\_/ \___/|_|  |_|\__,_(_|_)
+                                                         
+                                                         
