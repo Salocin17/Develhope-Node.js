a4206a5d9e563c422b0587e1340eab93966fef66
Nicolas Zarone <nikolaszarone@gmail.com>
2023-05-16 23:48:47 +0200

    CRUD

--- dist/server.js
+++ dist/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import morgan from 'morgan';
 import 'express-async-errors';
+import Joi from 'joi';
 const app = express();
 const port = 4000;
 app.use(express.json());
@@ -15,9 +16,39 @@ let planets = [
         name: "Mars",
     },
 ];
-app.get("/planets", (req, res) => {
+app.get("/api/planets", (req, res) => {
     res.status(200).json(planets);
 });
+app.get('/api/planets/:id', (req, res) => {
+    const { id } = req.params;
+    const planet = planets.find((planet) => planet.id === Number(id));
+    res.status(200).json(planet);
+});
+const planetSchema = Joi.object({
+    id: Joi.number().required(),
+    name: Joi.string().required(),
+});
+app.post('/api/planets', (req, res) => {
+    const { error } = planetSchema.validate(req.body);
+    if (error) {
+        return res.status(400).json({ error: error.details[0].message });
+    }
+    const { id, name } = req.body;
+    const newPlanet = { id, name };
+    planets = [...planets, newPlanet];
+    res.status(201).json({ msg: 'planet added' });
+});
+app.put('/api/planets/:id', (req, res) => {
+    const { id } = req.params;
+    const { name } = req.body;
+    planets = planets.map((planet) => planet.id === Number(id) ? Object.assign(Object.assign({}, planet), { name }) : planet);
+    res.status(200).json({ msg: 'planet updated' });
+});
+app.delete('/api/planets/:id', (req, res) => {
+    const { id } = req.params;
+    planets = planets.filter(planet => planet.id !== Number(id));
+    res.status(200).json({ msg: 'Planet Deleted' });
+});
 app.listen(port, () => {
-    console.log(`Listening on port${port}`);
+    console.log(`Listening on port ${port}`);
 });
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import morgan from 'morgan';
 import 'express-async-errors';
+import Joi from 'joi';
 
 const app = express();
 const port = 4000;
@@ -26,13 +27,47 @@ let planets: Planets = [
     },
 ];
 
-
-app.get("/planets", (req, res) => {
+app.get("/api/planets", (req, res) => {
     res.status(200).json(planets);
 });
 
+app.get('/api/planets/:id', (req, res) => {
+    const { id } = req.params;
+    const planet = planets.find((planet) => planet.id === Number(id));
+    res.status(200).json(planet);
+});
 
+const planetSchema = Joi.object({
+    id: Joi.number().required(),
+    name: Joi.string().required(),
+});
+
+app.post('/api/planets', (req, res) => {
+    const { error } = planetSchema.validate(req.body);
+
+    if (error) {
+        return res.status(400).json({ error: error.details[0].message });
+    }
+
+    const { id, name } = req.body;
+    const newPlanet = { id, name };
+    planets = [...planets, newPlanet];
+    res.status(201).json({ msg: 'planet added' });
+});
+
+app.put('/api/planets/:id', (req, res) => {
+    const { id } = req.params;
+    const { name } = req.body;
+    planets = planets.map((planet) => planet.id === Number(id) ? { ...planet, name } : planet);
+    res.status(200).json({ msg: 'planet updated' });
+});
+
+app.delete('/api/planets/:id', (req, res) => {
+    const { id } = req.params;
+    planets = planets.filter(planet => planet.id !== Number(id));
+    res.status(200).json({ msg: 'Planet Deleted' });
+});
 
 app.listen(port, () => {
-    console.log(`Listening on port${port}`);
+    console.log(`Listening on port ${port}`);
 });
